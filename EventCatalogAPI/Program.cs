df831e42607ea8208e174526f673fb9a84cfcd5d
allie <allielathrop@gmail.com>
2019-07-13 09:50:59 -0700

    slight edit to Program.cs

--- EventCatalogAPI/Program.cs
+++ EventCatalogAPI/Program.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using EventCatalogAPI.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using EventCatalogAPI.Data;
 
 namespace EventCatalogAPI
 {
