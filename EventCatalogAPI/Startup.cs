76052df8f263e185a713a713e71f346884ddca0b
allie <allielathrop@gmail.com>
2019-07-13 15:57:39 -0700

    add Swashbuckle to the Nuget package and change startup.cs code- including Swagger

--- EventCatalogAPI/Startup.cs
+++ EventCatalogAPI/Startup.cs
@@ -27,10 +27,23 @@ namespace EventCatalogAPI
         public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Configuration["ConnectionString"];
-
-            services.AddDbContext<CatalogContext>(options => options.UseSqlServer(connectionString));
-
+            services.AddDbContext<CatalogContext>(options =>
+                    options.UseSqlServer(connectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1",
+                    new Swashbuckle.AspNetCore.Swagger.Info
+                    {
+                        Title = "EventBrite - EventCatalogAPI",
+                        Version = "V1",
+                        Description = "Event catalog",
+                        TermsOfService = "Terms of Service"
+                    });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +53,11 @@ namespace EventCatalogAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"/swagger/v1/swagger.json", "EventCatalogAPI V1");
+                });
             app.UseMvc();
         }
     }
