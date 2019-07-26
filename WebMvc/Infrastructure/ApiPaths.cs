49126685adb7d5f1148bf5ba01f87278210629d7
Hy Huynh <hyxhuynh@gmail.com>
2019-07-26 15:54:12 -0700

    started WebMvc with Infrastructure, Models, and Services

---
+++ WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static class Catalog
+        {
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}catalogtypes";
+            }
+            public static string GetAllCategories(string baseUri)
+            {
+                return $"{baseUri}catalogcatagories";
+            }
+            public static string GetAllEventCities(string baseUri)
+            {
+                return $"{baseUri}catalogeventcities";
+            }
+            // ============= Remove Zipcode here if not needed ==============
+            public static string GetAllEventZipcodes(string baseUri)
+            {
+                return $"{baseUri}catalogeventzipcodes";
+            }
+            public static string GetAllCatalogEvents(string baseUri, int page, int take, int? type, int? category, int? eventcity, int? eventzipcode)
+            {
+                var filterQs = string.Empty;
+
+                if (category.HasValue || type.HasValue || eventcity.HasValue || eventzipcode.HasValue)
+                {
+                    var categoryQs = (category.HasValue) ? category.Value.ToString() : "null";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                    var eventcityQs = (eventcity.HasValue) ? eventcity.Value.ToString() : "null";
+                    var eventzipcodeQs = (eventzipcode.HasValue) ? eventzipcode.Value.ToString() : "null";
+                    filterQs = $"/type/{typeQs}/category/{categoryQs}/eventzipcode/{eventcityQs}/eventzipcode/{eventzipcodeQs}";
+                }
+
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+        }
+    }
+}
---
+++ WebMvc/Services/CatalogService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WebMvc.Infrastructure;
+using WebMvc.Models;
+
+namespace WebMvc.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly IHttpClient _client;
+        private readonly string _baseUri;
+
+        public CatalogService(IHttpClient httpclient,
+            IConfiguration config)
+        {
+            _client = httpclient;
+            _baseUri = $"{config["CatalogUrl"]}/api/catalog/";
+        }
+        public async Task<Catalog> GetCatalogEventsAsync(int page, int size, int? category, int? type, int? eventcity, int? eventzipcode)
+        {
+            var catalogEventsUri = ApiPaths.Catalog
+                            .GetAllCatalogEvents(_baseUri,
+                                page, size, category, type, eventcity, eventzipcode);
+
+            var dataString = await _client.GetStringAsync(catalogEventsUri);
+            var response = JsonConvert.DeserializeObject<Catalog>(dataString);
+            return response;
+        }
+
+        // Continue here ==================
+        public Task<IEnumerable<SelectListItem>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SelectListItem>> GetEventCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SelectListItem>> GetEventZipcodesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SelectListItem>> GetTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
