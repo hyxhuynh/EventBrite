c7995fd08ecef08c6adb4840398959fb413fb8dd
allie <allielathrop@gmail.com>
2019-07-26 16:31:28 -0700

    WebMVC project added. Dockercompose edited to reflect new WebMVC.

---
+++ WebMvc/Controllers/CatalogController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Services;
+using WebMvc.ViewModels;
+
+namespace WebMvc.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ICatalogService _service;
+        public CatalogController(ICatalogService service) =>
+            _service = service;
+
+        public async Task<IActionResult> Index(int? type, int? category, int? zipcode, int? city, int? page)
+        {
+            var itemsOnPage = 6;
+            var catalog =
+                await _service.GetCatalogEventsAsync(page ?? 0,
+                itemsOnPage, type, category, zipcode, city);
+
+            var vm = new CatalogIndexViewModel
+            {
+                PaginationInfo = new PaginationInfo
+                {
+                    ActualPage = page ?? 0,
+                    ItemsPerPage = itemsOnPage,
+                    TotalItems = catalog.Count,
+                    TotalPages = (int)Math.Ceiling((decimal)catalog.Count / itemsOnPage)
+                },
+                CatalogEvents = catalog.Data,
+                Types = await _service.GetTypesAsync(),
+                Categories = await _service.GetCategoriesAsync(),
+                ZipCodes = await _service.GetZipCodesAsync(),
+                Cities = await _service.GetCitiesAsync(),
+                TypesFilterApplied = type ?? 0,
+                CategoriesFilterApplied = category ?? 0,
+                ZipCodesFilterApplied = zipcode ?? 0,
+                CitiesFilterApplied = city ?? 0
+            };
+
+            vm.PaginationInfo.Previous = (vm.PaginationInfo.ActualPage == 0) ? "is-disabled" : "";
+            vm.PaginationInfo.Next = (vm.PaginationInfo.ActualPage == vm.PaginationInfo.TotalPages - 1) ? "is-disabled" : "";
+
+            return View(vm);
+        }
+    }
+}
---
+++ WebMvc/ViewModels/CatalogIndexViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models;
+
+namespace WebMvc.ViewModels
+{
+    public class CatalogIndexViewModel
+    {
+        public PaginationInfo PaginationInfo { get; set; }
+        public IEnumerable<SelectListItem> Types { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<SelectListItem> ZipCodes { get; set; }
+        public IEnumerable<SelectListItem> Cities { get; set; }
+        public IEnumerable<CatalogEvent> CatalogEvents { get; set; }
+
+        public int? TypesFilterApplied { get; set; }
+        public int? CategoriesFilterApplied { get; set; }
+        public int? ZipCodesFilterApplied { get; set; }
+        public int? CitiesFilterApplied { get; set; }
+
+    }
+}
