c863f18d966f189e491a4bf86768ba450ab38f52
Hy Huynh <hyxhuynh@gmail.com>
2019-08-08 16:14:44 -0700

    added CartApi Service

--- TokenServiceApi/wwwroot/lib/jquery-validation/dist/additional-methods.js
+++ TokenServiceApi/wwwroot/lib/jquery-validation/dist/additional-methods.js
@@ -942,13 +942,13 @@ $.validator.addMethod( "postcodeUK", function( value, element ) {
  *
  * The end result is that neither of these inputs:
  *
- *	<input class="productinfo" name="partnumber">
- *	<input class="productinfo" name="description">
+ *	<input class="eventinfo" name="partnumber">
+ *	<input class="eventinfo" name="description">
  *
  *	...will validate unless at least one of them is filled.
  *
- * partnumber:	{require_from_group: [1,".productinfo"]},
- * description: {require_from_group: [1,".productinfo"]}
+ * partnumber:	{require_from_group: [1,".eventinfo"]},
+ * description: {require_from_group: [1,".eventinfo"]}
  *
  * options[0]: number of fields that must be filled in the group
  * options[1]: CSS selector that defines the group of conditionally required fields
@@ -981,16 +981,16 @@ $.validator.addMethod( "require_from_group", function( value, element, options )
  *
  * The end result, is that none of these inputs:
  *
- *	<input class="productinfo" name="partnumber">
- *	<input class="productinfo" name="description">
- *	<input class="productinfo" name="color">
+ *	<input class="eventinfo" name="partnumber">
+ *	<input class="eventinfo" name="description">
+ *	<input class="eventinfo" name="color">
  *
  *	...will validate unless either at least two of them are filled,
  *	OR none of them are.
  *
- * partnumber:	{skip_or_fill_minimum: [2,".productinfo"]},
- * description: {skip_or_fill_minimum: [2,".productinfo"]},
- * color:		{skip_or_fill_minimum: [2,".productinfo"]}
+ * partnumber:	{skip_or_fill_minimum: [2,".eventinfo"]},
+ * description: {skip_or_fill_minimum: [2,".eventinfo"]},
+ * color:		{skip_or_fill_minimum: [2,".eventinfo"]}
  *
  * options[0]: number of fields that must be filled in the group
  * options[1]: CSS selector that defines the group of conditionally required fields
